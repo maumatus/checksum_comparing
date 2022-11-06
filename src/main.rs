f190cae338307abf0b50ffb83a860179bed6e8f3
Mauricio Matus Furniel <mauriciomatusfurniel@Mauricios-iMac.local>
2022-11-06 20:54:06 -0300

    Commit01-rama B: Logramos que leyera linea por linea, ahora falta comparar

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,19 @@
 use std::fs::File;
-use std::io::prelude::*;
+use std::io::BufRead;
+use std::io::BufReader;
+use std::path::Path;
+use differ::{Differ, Tag};
 
-// Partimos creando un checksum compare con "Strings" completos"
-// Luego haremos refactor para que diga cual archivo no es igual
 fn main() {
-    let mut file_a = File::open("md5sums_1.txt")
-    .expect("Archivo no encontrado");
-    let mut file_b = File::open("md5sums_2.txt")
-    .expect("Archivo no encontrado");
-    
-    let mut data_a = String::new();
-    let mut data_b = String::new();
-    
-    file_a.read_to_string(&mut data_a)
-    .expect("Error leyendo el archivo");
-    
-    file_b.read_to_string(&mut data_b)
-    .expect("Error leyendo el archivo");
-    
-    if data_a == data_b {
-        println!("Grupo de archivos coinciden");
-    } else {
-        println!("Grupo de archivos no coinciden");
-    }
+    let file_a = File::open(Path::new("md5sums_1.txt")).unwrap();
+    let reader_a = BufReader::new(&file_a);
+
+    let file_b = File::open(Path::new("md5sums_1.txt")).unwrap();
+    let reader_b = BufReader::new(&file_b);
+
+    let lines_a: Vec<String> = reader_a.lines().collect::<Result<_, _>>().unwrap();
+    let lines_b: Vec<String> = reader_b.lines().collect::<Result<_, _>>().unwrap();
+
+    print!("{:?}",lines_a);
+    print!("{:?}",lines_b);
 }
